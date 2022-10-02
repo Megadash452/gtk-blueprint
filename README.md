d1bc45f6d32bc92c776ded7f23d1164d1bec57d8
Megadash452 <marticraft680@gmail.com>
2022-10-01 22:29:30 -0400

    Blueprint Map

--- README.md
+++ README.md
@@ -2,17 +2,51 @@
 
 This crate lets you use the [Blueprint](https://jwestman.pages.gitlab.gnome.org/blueprint-compiler/) syntax for the UI in GTK applications built with Rust.
 
-For this to work the Blueprint Compiler directory must be found in the $PATH environtment variable or it can be included in the project root in `blueprint-compiler/blueprint-compiler.py`. If the Blueprint Compiler is not found your project will not compile.
+For this to work the Blueprint Compiler directory must be found in the **$PATH** environtment variable as *"blueprint-compiler"*, or it can be included in the project root in `blueprint-compiler/blueprint-compiler.py`.
+
+If the Blueprint Compiler is not found, your project will not compile. If the Blueprint compiler fails to compile the file to UI XML for whatever reason (file not found, bad syntax, etc.) your program will also fail to compile.
 
 ----
 
-Use the `include_blp!()` macro to compile the Blueprint to the format that GtkBuilder can read (UI XML). The macro takes a file path relative to the *project root*. Similarly to how the `include_str!()` macro works, the compiled UI XML is included in the Rust source file as a `&'static str`.
+## Include Blueprint
 
-If the Blueprint compiler fails to compile the file to UI XML for whatever reason (file not found, bad syntax, etc.) your program will also fail to compile.
+Use the `include_blp!()` macro to compile the Blueprint to the format that GtkBuilder can read (UI XML). The macro takes a file path relative to the *project root*. Similarly to how the `include_str!()` macro works, the compiled UI XML is embedded in the Rust source file as a `&'static str`.
 
-## Example
+### Example
 
 ```rust
-let builder = gtk::Builder::new();
-builder.add_from_string(include_blp!("./src/window.blp")).expect("error parsing UI XML");
+let builder = gtk::Builder::from_string(include_blp!("./src/window.blp"));
+// OR without ./ prefixed to the path
+let builder = gtk::Builder::from_string(include_blp!("src/window.blp"));
+```
+
+## Blueprint Map
+
+This macro generates a static [Map](https://docs.rs/phf/0.11.1/phf/struct.Map.html) of all the blueprints (files ending in `.blp`) in your project. It compiles the files and uses them as the *Values* for the map, with the *Keys* being the path of that blueprint relative to the *Project Root*. Then use [get_blp](https://docs.rs/gtk-blueprint/latest/gtk_blueprint/macro.get_blp.html) to use the `&str` of the compiled blueprint.
+
+The input for this macro is the path (relative to the *Project Root*) that it should start looking for blueprint files.
+
+The advantage of this is when there is a blueprint that is used multiple times in your code, it will only be embedded once in the binary, where [include_blp](https://docs.rs/gtk-blueprint/latest/gtk_blueprint/macro.get_blp.html) would embed it as many times as it is used.
+
+### Example
+
+```rust
+use gtk_blueprint::get_blp;
+
+gtk_blueprint::gen_blp_map!("");
+
+fn main() {
+    let builder = gtk::Builder::from_string(get_blp!("./src/window.blp"));
+    // OR without ./ prefixed to the path
+    let builder = gtk::Builder::from_string(get_blp!("src/window.blp"));
+}
+```
+
+## Bug
+
+If you make changes to any of your Blueprint files but don't make changes to the Rust code, cargo will think that it does not need to recompile the project because the code hasn't changed and it will run with the old version of the blueprint. As a work-around, create a file `build.rs` at the root of your project and put this in it:
+```rust
+fn main() {
+    println!("cargo:rerun-if-changed=**/*.blp");
+}
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -2,52 +2,142 @@ mod util;
 
 use util::*;
 use proc_macro::{self, TokenStream};
-use syn::LitStr;
+use syn::{LitStr, parse_macro_input};
+
+const BLP_MAP_NAME: &str = "__COMPILED_BLUEPRINT_MAP__";
+
+
+// TODO: #![doc(include="../README.md")] when it comes to stable
 
 
 #[proc_macro]
-/// Includes a GtkUI compiled from a Blueprint file as a string.
+/// Embeds a GtkUI compiled from a Blueprint file as a string to your Rust code.
+/// 
+/// This is meant to be used if the blueprint will only be used *once*. Otherwise
+/// please see [gen_blp_map!].
+/// 
+/// ### Input
 ///
 /// The file is located relative to the *Project Root* directory. The provided path
 /// is interpreted in a platform-specific way at compile time. So, for instance, an
 /// invocation with a Windows path containing backslashes \ would not compile
 /// correctly on Unix.
 /// 
-/// Will try to invoke the compiler if it is in **$PATH** or if a directory named
-/// "blueprint-compiler" with *blueprint-compiler.py* is found in the *Project Root*.
-///
+/// ### Output
+/// 
 /// This macro will yield an expression of type `&'static str` which is the compiled
 /// UI XML of the Blueprint.
+/// 
+/// ## Error
+/// 
+/// If the Blueprint compiler fails to compile the file to UI XML for whatever
+/// reason (file not found, bad syntax, etc.) your program will also fail to compile.
 pub fn include_blp(input: TokenStream) -> TokenStream {
     // TODO: trigger a rebuild if .blp file has changed
-    let ast: LitStr = syn::parse(input).unwrap();
+    let path = parse_macro_input!(input as LitStr).value();
     
-    match compile_blp(&ast.value()) {
-        Ok(xml) => format!("r###\"{}\"###", xml).parse().unwrap(),
-        // TODO: compile_error!() instead of panic!()
-        Err(error) => panic!("blueprint-compiler error: {}", error)
+    match compile_blp(&path) {
+        Ok(xml) => format!("r###\"{xml}\"###").parse().unwrap(),
+        Err(error) => format!("compile_error!(\"{error}\")").parse().unwrap()
     }
 }
 
-// #[proc_macro]
-// Compile a list of Blueprints into GtkUI and put them in a phf map
-// pub fn blueprint_map(input: TokenStream) -> TokenStream {
-    /*for path in search_blps(current_dir.to_str().unwrap()) {
-        // If is absolute path, make relative path if possible
-        let path = match path.strip_prefix(
-            &format!("{}/", current_dir.to_str().unwrap())
-        ) {
-            Some(path) => String::from(path),
-            None => path
-        };
+
+#[proc_macro]
+/// Generates a [Map](https://docs.rs/phf/0.11.1/phf/struct.Map.html) of all the
+/// blueprints in the project and their paths. ***Requires crate
+/// [phf](https://crates.io/crates/phf) as a dependency in your project.***
+/// 
+/// This is meant to be used if the blueprint will be used *multiple times*.
+/// Otherwise you can use [include_blp!].
+/// 
+/// The **Keys** are the paths to each of the blueprints that were compiled into the
+/// Map. The **Values** are the compiled blueprints.
+/// 
+/// ## Use
+/// 
+/// Use the macro in the root of your `main.rs`:
+/// 
+/// ```rs
+/// gtk_blueprint::gen_blp_map!("");
+/// 
+/// fn main() {
+///     
+/// }
+/// ```
+/// 
+/// ### Input
+/// 
+/// The macro takes a **starting directory** relative to the *Project Root* as input.
+/// The macro looks for `.blp` files recursively starting from that directory.
+/// An empty string as input means to use the *Project Root* as the starting
+/// directory.
+/// 
+/// To use the compiled blueprint in your code, use the [get_blp!] macro, giving it
+/// the *blueprint's path* as input.
+/// 
+/// ### Output
+/// 
+/// The generated Map is a static global variable named `__COMPILED_BLUEPRINT_MAP__`.
+/// The underscores are there to prevent the name from conflicting with some other
+/// variable or crate. However, there is no need to use the static variable. It is
+/// only there so it can be used by the [get_blp!] macro.
+/// 
+/// -------
+/// 
+/// On a project structured like this:
+/// ```txt
+///  │
+///  ├ root.blp
+///  └ src/
+///    ├ main.rs
+///    ├ src.blp
+///    └ util/
+///      ├ mod.rs
+///      └ mod.blp
+/// ```
+/// The map would look like this:
+/// ```rs
+/// Map {
+///   "root.blp" => "...",
+///   "src/src.blp" => "...",
+///   "src/util/mod.blp" => "..."
+/// }
+/// ```
+/// Then use the compiled blueprint with `gtk::Builder`:
+/// ```rs
+/// let builder = gtk::Builder::from_string(get_blp!("src/src.blp"));
+/// ```
+/// ------
+/// 
+/// 
+/// ## Error
+/// 
+/// If the Blueprint Compiler fails to compile any of the .blp files, your program
+/// will fail to compile.
+pub fn gen_blp_map(input: TokenStream) -> TokenStream {
+    let mut map = phf_codegen::Map::<String>::new();
+    let mut errors = false;
+
+    let start_path = {
+        let path = parse_macro_input!(input as LitStr).value();
+
+        // search_blps() can't take an empty path
+        if path.is_empty() {
+            ".".to_string()
+        } else {
+            path
+        }
+    };
+    
+    for mut path in search_blps(&start_path) {
         // Remove the "./" if it exists in the path
-        let path = match path.strip_prefix("./") {
-            Some(path) => String::from(path),
-            None => path
-        };
+        if let Some(new_path) = path.strip_prefix("./") {
+            path = String::from(new_path);
+        }
 
         match compile_blp(&path) {
-            Ok(xml) => map.entry(path, &xml),
+            Ok(xml) => map.entry(path, &format!("r###\"{xml}\"###")),
             Err(error) => {
                 errors = true;
                 eprintln!("{error}");
@@ -55,5 +145,36 @@ pub fn include_blp(input: TokenStream) -> TokenStream {
             }
         };
     }
-    format!("pub static COMPILED_BLPS: phf::Map<&'static str, &'static str> = \n{};\n", map.build())*/
-// }
+
+    if errors {
+        format!("compile_error!(\"One or more Blueprints had errors and could not be compiled. Check output of `cargo build` for more details`\")").parse().unwrap()
+    } else {
+        format!("pub static {BLP_MAP_NAME}: phf::Map<&'static str, &'static str> = {};\n", map.build()).parse().unwrap()
+    }
+}
+
+#[proc_macro]
+/// Get the compiled blueprints generated by [gen_blp_map!]
+/// 
+/// The macro takes the path of the blueprint relative to the *Project Root* as
+/// input. Macro results in something like `__COMPILED_BLUEPRINT_MAP__.get("path").unwrap()`,
+/// and that returns the `&'static str` that is the compiled blueprint.
+pub fn get_blp(input: TokenStream) -> TokenStream {
+    let path = {
+        let path = parse_macro_input!(input as LitStr).value();
+
+        /* Remove the "./" if it exists in the path.
+          The keys in the map will have this format: src/blp */
+        if let Some(path) = path.strip_prefix("./") {
+            String::from(path)
+        } else {
+            path
+        }
+    };
+
+    if std::path::Path::new(&path).exists() {
+        format!("{BLP_MAP_NAME}.get(\"{path}\").expect(\"Blueprint did not compile correctly\")").parse().unwrap()
+    } else {
+        format!("compile_error!(\"Error getting blueprint \\\"{path}\\\": File Not Found.\")").parse().unwrap()
+    }
+}
